bfb2104229257f10511bb7e9bf168882a80b6728
Richard-Moura <richardsilva_moura@hotmail.com>
2025-12-15 21:38:30 -0300

    Resolve conflitos e prepara build para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tela-login-react/' // necessário para GitHub Pages
+  base: '/tela-login-react/' // caminho do seu repositório
 });
